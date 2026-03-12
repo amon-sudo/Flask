74c4b13f2edc5582043403c018ff6baba67c5109
amon-sudo <amonogino@gmail.com>
2026-03-12 23:45:02 +0300

    added a debug == true in the app.run func, it eas so cool to do that

--- flask_tutorial.py
+++ flask_tutorial.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route("/<name>")
 # defining the pages
 def home(name):
-    return render_template("index.html", content =["Amon", "Glen", "James"] )
+    return render_template("index.html", content =)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
