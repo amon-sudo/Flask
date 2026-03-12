a55373e244abd5b51be177eb67bbf46b0a95762b
amon-sudo <amonogino@gmail.com>
2026-03-12 20:42:39 +0300

    huh

--- flask_tutorial.py
+++ flask_tutorial.py
@@ -3,10 +3,10 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 # route.
-@app.route("/")
+@app.route("/<name>")
 # defining the pages
-def home():
-    return render_template("index.html")
+def home(name):
+    return render_template("index.html", content =["Amon", "Glen", "James"] )
 
 
 if __name__ == "__main__":
