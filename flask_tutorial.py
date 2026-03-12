55ff9652d63ddc12a8bfa506d552f1b2d5a4de71
amon-sudo <amonogino@gmail.com>
2026-03-13 01:05:57 +0300

    troubled with this shit

--- flask_tutorial.py
+++ flask_tutorial.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -11,7 +11,12 @@ def home(name):
 @app.route("/login", methods= ["POST", "GET"])
 
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        user = request.form["nam"]
+        return redirect(url_for("user", usr= user))
+    else:
+            
+        return render_template("login.html")
 
 
 @app.route("/<usr>")
