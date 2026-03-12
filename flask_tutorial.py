740513f10a57c08c9b21c13bb7ee51d5ebbed6de
amon-sudo <amonogino@gmail.com>
2026-03-12 19:00:29 +0300

    created first flask program

--- flask_tutorial.py
+++ flask_tutorial.py
@@ -1,1 +1,19 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+# route.
+@app.route("/")
+# defining the pages
+def home():
+    return "<h1>Hello this is the main page</h1>"
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}"
+
+@app.route("/admin")
+def admin ():
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run()
