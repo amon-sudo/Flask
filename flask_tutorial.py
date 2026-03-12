ed684e03c2a7412f7fb9904523b1bd89f80d24fb
amon-sudo <amonogino@gmail.com>
2026-03-13 01:12:32 +0300

    d1ne wit it

--- flask_tutorial.py
+++ flask_tutorial.py
@@ -3,9 +3,9 @@ from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 # route.
-@app.route("/<name>")
+@app.route("/")
 # defining the pages
-def home(name):
+def home():
     return render_template("index.html" )
 
 @app.route("/login", methods= ["POST", "GET"])
