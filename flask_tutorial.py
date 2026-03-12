5989912c4b9fa5f4482b96eb402c49236bd31fef
amon-sudo <amonogino@gmail.com>
2026-03-13 00:31:20 +0300

    trying them http methods in flask. im not fun

--- flask_tutorial.py
+++ flask_tutorial.py
@@ -6,7 +6,18 @@ app = Flask(__name__)
 @app.route("/<name>")
 # defining the pages
 def home(name):
-    return render_template("index.html", content ="Amon" )
+    return render_template("index.html" )
+
+@app.route("/login", method= ["POST", "GET"])
+
+def login():
+    return render_template("")
+
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}<h1>"
+    
 
 
 if __name__ == "__main__":
