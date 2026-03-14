82a1af0cf19bdeb6b4a114dcc6fc368fe8fcc265
amon-sudo <amonogino@gmail.com>
2026-03-14 20:07:21 +0300

    sql alchemy

--- flask_tutorial.py
+++ flask_tutorial.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
-import flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "hello"
