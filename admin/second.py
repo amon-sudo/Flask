eb83cc93d635a2f10eebac7ae3306ec348fcbcc9
amon-sudo <amonogino@gmail.com>
2026-03-17 22:56:13 +0300

    ff

--- admin/second.py
+++ admin/second.py
@@ -6,7 +6,7 @@ second = Blueprint("second", __name__, static_folder="static", template_folder="
 @second.route("/home")
 @second.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", "login.html")
 
 
 @second.route("/test")
